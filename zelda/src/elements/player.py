cf72de397e2e4b1755ba022d9d535cab73e528cb
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-01-07 14:27:48 -0300

    docs: adiciona docstrings para a classe do player

--- zelda/src/elements/player.py
+++ zelda/src/elements/player.py
@@ -7,10 +7,24 @@ from zelda.src.settings import BASE_PATH
 
 
 class Player(Sprite):
+    """Sprite que representa o player do jogo.
+
+    Essa classe lida com as animações e captura de entradas feitas pelo
+    usuário para o player.
+    """
 
     def __init__(self,
                  position: Tuple[float, float],
                  groups: Union[List[AbstractGroup], AbstractGroup]) -> None:
+        """Faz o setup básico do player
+
+        Args:
+            position (Tuple[float, float]):
+                posição do player na tela, em pixels
+            groups (Union[List[AbstractGroup], AbstractGroup]):
+                grupos que o player deve pertencer quando for utilizado
+                no jogo
+        """
         super().__init__(groups)
 
         self.image = load_image(
