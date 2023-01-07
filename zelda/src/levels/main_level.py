6c736c5c9321978a0c6e28219badf36ca6494ec3
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-01-07 14:07:44 -0300

    feat: adiciona método para a criação do mapa

--- zelda/src/levels/main_level.py
+++ zelda/src/levels/main_level.py
@@ -1,6 +1,9 @@
 import pygame
 
 from zelda.src.levels.abstract_level import AbstractLevel
+from zelda.src.settings import WORLD_MAP, TILESIZE
+from zelda.src.elements.player import Player
+from zelda.src.elements.map.tile import Tile
 
 
 class MainLevel(AbstractLevel):
@@ -12,5 +15,22 @@ class MainLevel(AbstractLevel):
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        # Setup dos sprites
+        self.__create_map()
+
+    def __create_map(self) -> None:
+        for i, row in enumerate(WORLD_MAP):
+            for j, tile in enumerate(row):
+                x = j * TILESIZE
+                y = i * TILESIZE
+
+                if tile == 'p':
+                    Player((x, y), [self.visible_sprites])
+                    continue
+
+                if tile == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+
     def run(self) -> None:
-        pass
+        self.visible_sprites.update()
+        self.visible_sprites.draw(self.display_surface)
