1cd8f1cb6e48a75ed68859009f051c98793bc790
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-01-14 14:37:14 -0300

    feat: adiciona os dados das armas para o settings

--- zelda/src/settings.py
+++ zelda/src/settings.py
@@ -8,3 +8,11 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 FPS = 60
 TILESIZE = 64
+
+WEAPON_DATA = {
+    "sword": {"cooldown": 100, "damage": 15, "graphic": "sword/full.png"},
+    "lance": {"cooldown": 400, "damage": 30, "graphic": "lance/full.png"},
+    "axe": {"cooldown": 300, "damage": 20, "graphic": "axe/full.png"},
+    "rapier": {"cooldown": 50, "damage": 8, "graphic": "rapier/full.png"},
+    "sai": {"cooldown": 80, "damage": 10, "graphic": "sai/full.png"},
+}
