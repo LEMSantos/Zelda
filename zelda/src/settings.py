efc0c18a0254b9ca4a3594bb694b2696374d62ab
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-01-14 18:26:19 -0300

    feat: adiciona variáveis necessárias para a construção da UI

--- zelda/src/settings.py
+++ zelda/src/settings.py
@@ -9,10 +9,40 @@ SCREEN_HEIGHT = 720
 FPS = 60
 TILESIZE = 64
 
+# Cores gerais
+WATER_COLOR = '#71ddee'
+TEXT_COLOR = '#EEEEEE'
+
+# Parâmetros da UI
+UI_BAR_HEIGHT = 20
+UI_HEALTH_BAR_WIDTH = 200
+UI_ENERGY_BAR_WIDTH = 140
+UI_ITEM_BOX_SIZE = 80
+UI_FONT = f"{BASE_PATH}/graphics/font/joystix.ttf"
+UI_FONT_SIZE = 18
+
+# Cores da UI
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+UI_HEALTH_COLOR = 'red'
+UI_ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
+# Armas
 WEAPON_DATA = {
     "sword": {"cooldown": 100, "damage": 15, "graphic": "sword/full.png"},
     "lance": {"cooldown": 400, "damage": 30, "graphic": "lance/full.png"},
     "axe": {"cooldown": 300, "damage": 20, "graphic": "axe/full.png"},
     "rapier": {"cooldown": 50, "damage": 8, "graphic": "rapier/full.png"},
     "sai": {"cooldown": 80, "damage": 10, "graphic": "sai/full.png"},
+}
+
+
+# Player
+PLAYER_BASE_STATS = {
+    "health": 100,
+    "energy": 60,
+    "attack": 10,
+    "magic": 4,
+    "speed": 6,
 }
