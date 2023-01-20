7d63b6e213ac8f77fcd1dd91899a6bc3c7235897
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-01-20 13:10:45 -0300

    feat: adiciona classe para lidar com o uso de magias

---
+++ zelda/src/elements/magic.py
@@ -0,0 +1,98 @@
+from random import randint
+from typing import Union, List
+
+from pygame.math import Vector2
+from pygame.sprite import AbstractGroup
+
+from zelda.src.settings import TILESIZE
+from zelda.src.elements.player import Player
+from zelda.src.core.particle_effect import AnimationPlayer
+
+
+class MagicPlayer:
+    """Classe que representa um método para execução das animações
+    relacionadas ao uso de magias.
+
+    Essa classe lida tanto com as animações, quanto com a manutenção
+    da vida e energia do player ao utilizar magia.
+    """
+
+    def __init__(self, animation_player: AnimationPlayer) -> None:
+        """Inicializa a classe MagicPlayer.
+
+        Args:
+            animation_player (AnimationPlayer):
+                player responsável por executar as animações das partículas
+        """
+        self.__animation_player = animation_player
+
+    def heal(self,
+             player: Player,
+             strength: int,
+             cost: int,
+             groups: Union[List[AbstractGroup], AbstractGroup]) -> None:
+        """Gerencia as animações relacionadas a magia de cura.
+
+        Args:
+            player (Player):
+                instância do player no jogo
+            strength (int):
+                quantidade de vida que será curada
+            cost (int):
+                custo de energia para utilizar a magia
+            groups (Union[List[AbstractGroup], AbstractGroup]):
+                grupos de sprites em que as animações devem estar
+        """
+        if player.energy >= cost:
+            player.energy -= cost
+            player.health += strength
+
+            heal_offset = Vector2(0, -60)
+            self.__animation_player.create_particles(
+                name="heal",
+                position=player.rect.center + heal_offset,
+                groups=groups,
+            )
+
+            self.__animation_player.create_particles(
+                name="aura",
+                position=player.rect.center,
+                groups=groups,
+            )
+
+    def flame(self,
+              player: Player,
+              cost: int,
+              groups: Union[List[AbstractGroup], AbstractGroup]) -> None:
+        """Gerencia as animações relacionadas a magia de fogo.
+
+        Args:
+            player (Player):
+                instância do player no jogo
+            cost (int):
+                custo de energia para utilizar a magia
+            groups (Union[List[AbstractGroup], AbstractGroup]):
+                grupos de sprites em que as animações devem estar
+        """
+        if player.energy >= cost:
+            player.energy -= cost
+
+            status = player.status.split("_")[0]
+            axis = "y" if status in ["up", "down"] else "x"
+            direction = -1 if status in ["up", "left"] else 1
+
+            for i in range(1, 6):
+                flame_offset = Vector2(
+                    randint(-TILESIZE // 3, TILESIZE // 3),
+                    randint(-TILESIZE // 3, TILESIZE // 3))
+
+                setattr(
+                    flame_offset,
+                    axis,
+                    getattr(flame_offset, axis) + direction * i * TILESIZE)
+
+                self.__animation_player.create_particles(
+                    name="flame",
+                    position=player.rect.center + flame_offset,
+                    groups=groups,
+                )
